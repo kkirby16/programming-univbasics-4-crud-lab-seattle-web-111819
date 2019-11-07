7b6f9913f72bda3fa158558cddd4da4c9cdd0a6e
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-07 06:04:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array(array, element) << "sausage"
+  add_element_to_end_of_array << "sausage"
 end
 
 def add_element_to_start_of_array(array, element)
